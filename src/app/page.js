d98c28783e8ba82190d91f760b69a66867346b10
tokhy24 <mohamed21346587@gmail.com>
2025-11-01 00:55:37 +0200

    Making website rendering client-side

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,4 @@
+"use client"
 
 import AboutSection from "./components/homepage/about";
 import ContactSection from "./components/homepage/contact";
@@ -9,7 +10,7 @@ import Skills from "./components/homepage/skills";
 
 
 
-export default async function Home() {
+export default function Home() {
     // browser code
     return (
       <>
--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -6,7 +6,7 @@ export const personalData = {
     "I'm a driven Full Stack Web Developer with a passion for building digital experiences that blend performance, design, and innovation. My expertise spans the MERN stack and modern frameworks like Next.js and Laravel, empowering me to create everything from sleek frontends to scalable backend architectures.\n\nI thrive on turning complex ideas into clean, functional code — crafting responsive, high-performing web apps with Tailwind CSS, TypeScript, and cutting-edge JavaScript tools. Beyond coding, I'm a natural problem solver and team collaborator who values precision, scalability, and user impact above all.\n\nEvery project I work on is an opportunity to push boundaries, learn something new, and build technology that actually matters.",
   email: "mohameashraf24@gmail.com",
   phone: "+201024143237",
-  address: "Qalubia, Cairo , Egypt ",
+  address: "Cairo , Egypt ",
   github: "https://github.com/tokhy1",
   facebook: "https://www.facebook.com/mohamed.ashraf.19357/",
   linkedIn:
