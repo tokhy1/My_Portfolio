34d0d27298fccd254af007db9992dc3f0ab29510
tokhy24 <mohamed21346587@gmail.com>
2025-10-31 23:07:14 +0200

    Update Education details

--- utils/data/educations.js
+++ utils/data/educations.js
@@ -3,12 +3,18 @@ export const educations = [
     id: 1,
     title: "Bachelor Degree",
     duration: "2024 - Present",
-    institution: "NTCU",
+    institution: "October Technological University - OTU",
   },
   {
     id: 2,
-    title: "Higher Secondary Certificate",
+    title: "International Diploma in Information Technology ",
+    duration: "2022 - 2024",
+    institution: "Pearson BTECh Level 3 IT Diploma ",
+  },
+  {
+    id: 3,
+    title: "Applied technology diploma",
     duration: "2021 - 2024",
     institution: "WE School For Applied Technology",
-  }
-]
+  },
+];
