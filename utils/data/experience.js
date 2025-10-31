cdebafc025c86143d3024dd790bbb5306f28f1ee
tokhy24 <mohamed21346587@gmail.com>
2025-10-31 23:14:00 +0200

    Update Experiences

--- utils/data/experience.js
+++ utils/data/experience.js
@@ -1,15 +1,26 @@
 export const experiences = [
-
+  {
+    id: 1,
+    title: "Coding Instructor",
+    company: "Creative Children Community (3C)",
+    duration: "(Jan 2025 - Present)",
+  },
   {
     id: 2,
-    title: "FullStack Developer",
-    company: "Webbrical",
-    duration: "(Aug 2022 - Sep 2024)"
+    title: "Software Engineering Trainee",
+    company: "Telecom Egypt",
+    duration: "(May 2023 - June 2023)",
   },
   {
     id: 3,
+    title: "Coding and Programming Trainee",
+    company: "Samsung Innovation Campus (SIC)",
+    duration: "(Abr 2022 - Aug 2022)",
+  },
+  {
+    id: 4,
     title: "Self Employed",
     company: "Code and build something in everyday.",
-    duration: "(Jan 2022 - Present)"
-  }
-]
+    duration: "(Jan 2022 - Present)",
+  },
+];
